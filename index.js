0dce4136d310b7a7f679e9bb4b6f8fafd1960ba0
woods-ming <woodsming@sina.com>
2019-01-11 16:40:55 +0800

    fix bug

--- index.js
+++ index.js
@@ -5,17 +5,15 @@ const through = require('through2');
 const ejs = require('ejs');
 const fs = require('fs');
 
-module.exports = function (options) {
-	return through.obj(function (file, enc, cb) {
-		if (file.isNull()) {
-	      next(null, file);
-	      return;
-	    }
+module.exports = options => {
+  return through.obj(function(file, enc, next) {
+    if (file.isNull()) {
+      next(null, file);
+      return;
+    }
 
-		if (file.isStream()) {
-			file.contents = file.contents.pipe(through(minify));
-		} else {
-			try {
+    const convert = (buf, _, cb) => {
+    	try {
 				var data = JSON.parse(file.contents.toString());
 	        	var str = fs.readFileSync(options.filename,'utf-8');
 				file.contents = new Buffer(ejs.render(str, data, options));
@@ -30,8 +28,12 @@ module.exports = function (options) {
 		        }
 		        cb(error);
 			}
+    };
 
-			cb();
-		}
-	});
+    if (file.isStream()) {
+      file.contents = file.contents.pipe(through(convert));
+    } else {
+      convert(file.contents, null, next);
+    }
+  });
 };
