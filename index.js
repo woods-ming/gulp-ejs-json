5228cdca4f36e10fbbf6ddf14e547e886bce953e
woods-ming <woodsming@sina.com>
2019-01-11 16:12:09 +0800

    change gulp-util

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 'use strict';
-var gutil = require('gulp-util');
-var through = require('through2');
-var ejs = require('ejs');
-var fs = require('fs');
+
+const PluginError = require('plugin-error');
+const through = require('through2');
+const ejs = require('ejs');
+const fs = require('fs');
 
 module.exports = function (options) {
-	if (!options.filename) {
-		throw new gutil.PluginError('gulp-ejs-json', '`filename` required');
-	}
-
 	return through.obj(function (file, enc, cb) {
 		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
+	      next(null, file);
+	      return;
+	    }
 
 		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-ejs-json', 'Streaming not supported'));
-			return;
-		}
+			file.contents = file.contents.pipe(through(minify));
+		} else {
+			try {
+				var data = JSON.parse(file.contents.toString());
+	        	var str = fs.readFileSync(options.filename,'utf-8');
+				file.contents = new Buffer(ejs.render(str, data, options));
+				file.path = gutil.replaceExtension(file.path, ".html");
+				this.push(file);
+			} catch (err) {
+				let opts = Object.assign({}, options, { fileName: file.path });
+		        let error = new PluginError('gulp-ejs-json', err, opts);
+		        if (next !== cb) {
+		          next(error);
+		          return;
+		        }
+		        cb(error);
+			}
 
-		try {
-			var data = JSON.parse(file.contents.toString());
-        	var str = fs.readFileSync(options.filename,'utf-8');
-			file.contents = new Buffer(ejs.render(str, data, options));
-			file.path = gutil.replaceExtension(file.path, ".html");
-			this.push(file);
-		} catch (err) {
-			this.emit('error', new gutil.PluginError('gulp-ejs-json', err));
+			cb();
 		}
-
-		cb();
 	});
 };
