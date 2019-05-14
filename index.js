58f47e1af37cd9f8ded4a02a0ee6f6239097563e
woods-ming <woodsming@sina.com>
2019-05-14 13:04:32 +0800

    test

--- index.js
+++ index.js
@@ -13,28 +13,19 @@ module.exports = options => {
       return;
     }
 
-    const convert = (buf, _, cb) => {
-    	try {
-				var data = JSON.parse(file.contents.toString());
-	        	var str = fs.readFileSync(options.filename,'utf-8');
-				file.contents = new Buffer(ejs.render(str, data, options));
-				file.path = replaceExt(file.path, ".html");
-				this.push(file);
-			} catch (err) {
-				let opts = Object.assign({}, options, { fileName: file.path });
-		        let error = new PluginError('gulp-ejs-json', err, opts);
-		        if (next !== cb) {
-		          next(error);
-		          return;
-		        }
-		        cb(error);
-			}
-    };
+    try {
+		var data = JSON.parse(file.contents.toString());
+    	var str = fs.readFileSync(options.filename,'utf-8');
+		file.contents = new Buffer(ejs.render(str, data, options));
+		file.path = replaceExt(file.path, ".html");
+		this.push(file);
+	} catch (err) {
+		let opts = Object.assign({}, options, { fileName: file.path });
+        let error = new PluginError('gulp-ejs-json', err, opts);
+        next(error);
+        return;
+	}
 
-    if (file.isStream()) {
-      file.contents = file.contents.pipe(through(convert));
-    } else {
-      convert(file.contents, null, next);
-    }
+	next();
   });
 };
