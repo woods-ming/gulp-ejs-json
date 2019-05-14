72d43bac3b0294c1e1701f15b5f44186157d60b8
woods-ming <woodsming@sina.com>
2019-05-14 13:13:01 +0800

    upgrade

--- index.js
+++ index.js
@@ -29,3 +29,31 @@ module.exports = options => {
 	next();
   });
 };
+module.exports = function (options) {
+	return through.obj(function (file, enc, cb) {
+		if (file.isNull()) {
+			cb(null, file);
+			return;
+		}
+
+		if (file.isStream()) {
+			cb(new PluginError('gulp-ejs-json', 'Streaming not supported'));
+			return;
+		}
+
+		try {
+			var data = JSON.parse(file.contents.toString());
+        	var str = fs.readFileSync(options.filename,'utf-8');
+			file.contents = new Buffer(ejs.render(str, data, options));
+			file.path = replaceExt(file.path, ".html");
+			this.push(file);
+		} catch (err) {
+			let opts = Object.assign({}, options, { fileName: file.path });
+	        let error = new PluginError('gulp-ejs-json', err, opts);
+	        cb(error);
+	        return;
+		}
+
+		cb();
+	});
+};
